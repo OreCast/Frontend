5ac0020d245cf6c8196fa351e0c13e36c224cec1
Valentin Kuznetsov <vkuznet@gmail.com>
2023-10-02 09:41:29 -0400

    Add new helper function to place http GET/POST requests with Authz token

--- middleware.go
+++ middleware.go
@@ -1,11 +1,20 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
 	"log"
 	"net/http"
+	"net/url"
+	"strings"
 
 	authz "github.com/OreCast/Authz/auth"
 	"github.com/gin-gonic/gin"
+	jwt "github.com/golang-jwt/jwt/v4"
+	cryptoutils "github.com/vkuznet/cryptoutils"
 )
 
 // _token is used across all authorized APIs
@@ -50,6 +59,7 @@ func AuthMiddleware() gin.HandlerFunc {
 	}
 }
 
+// helper function to refresh global token used in authorized APIs
 func refreshToken() error {
 	// check and obtain token
 	var err error
@@ -63,4 +73,93 @@ func refreshToken() error {
 		err = _token.Validate(Config.AuthzClientId)
 	}
 	return err
+}
+
+// helper function to obtain JWT token from OreCast Authz service
+func getToken() (authz.Token, error) {
+	var token authz.Token
+	rurl := fmt.Sprintf("%s/oauth/token?client_id=%s&client_secret=%s&grant_type=client_credentials&scope=read", Config.AuthzURL, Config.AuthzClientId, Config.AuthzClientSecret)
+	resp, err := http.Get(rurl)
+	defer resp.Body.Close()
+	data, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return token, err
+	}
+	err = json.Unmarshal(data, &token)
+	if err != nil {
+		return token, err
+	}
+	reqToken := token.AccessToken
+	if Config.Verbose > 0 {
+		log.Printf("INFO: obtain token %+v", token)
+	}
+
+	// validate our token
+	var jwtKey = []byte(Config.AuthzClientId)
+	claims := &authz.Claims{}
+	tkn, err := jwt.ParseWithClaims(reqToken, claims, func(token *jwt.Token) (any, error) {
+		return jwtKey, nil
+	})
+	if err != nil {
+		if err == jwt.ErrSignatureInvalid {
+			return token, errors.New("invalid token signature")
+		}
+		return token, err
+	}
+	if !tkn.Valid {
+		log.Println("WARNING: token invalid")
+		return token, errors.New("invalid token validity")
+	}
+	return token, nil
+}
+
+// helper function to perform HTTP GET request with bearer token
+func httpGet(rurl string) (*http.Response, error) {
+	req, err := http.NewRequest("GET", rurl, nil)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", _token.AccessToken))
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	return resp, err
+}
+
+// helper function to perform HTTP POST request with bearer token
+func httpPost(rurl, contentType string, buffer *bytes.Buffer) (*http.Response, error) {
+	req, err := http.NewRequest("POST", rurl, buffer)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", _token.AccessToken))
+	req.Header.Add("Content-Type", contentType)
+	req.Header.Add("Accept", contentType)
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	return resp, err
+}
+
+// helper function to perform HTTP POST form request with bearer token
+func httpPostForm(rurl string, formData url.Values) (*http.Response, error) {
+	req, err := http.NewRequest("POST", rurl, strings.NewReader(formData.Encode()))
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", _token.AccessToken))
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	return resp, err
+}
+
+// helper function to encrypt user attributes
+func encryptUserObject(form UserRegistrationForm) (UserRegistrationForm, error) {
+	encryptedObject, err := cryptoutils.HexEncrypt(
+		form.Password, Config.DiscoveryPassword, Config.DiscoveryCipher)
+	if err != nil {
+		return form, err
+	} else {
+		form.Password = encryptedObject
+	}
+	return form, nil
 }
